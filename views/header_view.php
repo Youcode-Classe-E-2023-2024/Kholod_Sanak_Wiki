b6d3bd127d1c847e03319ba36afa57949f212135
kholoud001 <kholoud.sanak@gmail.com>
2024-01-08 12:29:24 +0100

    header update

--- views/header_view.php
+++ views/header_view.php
@@ -32,7 +32,7 @@
         </div>
     </ul>
    <a class="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-xl transition duration-200" href="index.php?page=login">Sign In</a>
-   <a class="hidden lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" href="index.php?page=login">Sign up</a>
+   <a class="hidden lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" href="index.php?page=register">Sign up</a>
     <!-- when it is logged -->
 <!--    <a class="hidden lg:inline-block lg:ml-auto lg:mr-3  py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white  font-bold  rounded-xl transition duration-200" href="index.php?page=listWiki">Manage Wikis</a>-->
 <!--    <a class="hidden lg:inline-block py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900  font-bold rounded-xl transition duration-200" href="index.php?page=logout">Logout</a>-->
