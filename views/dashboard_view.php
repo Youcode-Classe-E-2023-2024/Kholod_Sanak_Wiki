b3c9f5f6b3d4ea26f54b1752bba3c1bbef3cf01f
kholoud001 <kholoud.sanak@gmail.com>
2024-01-08 17:32:27 +0100

    dashboard update

--- views/dashboard_view.php
+++ views/dashboard_view.php
@@ -35,7 +35,7 @@
                         <!-- category-->
                         <li class="relative px-2 py-1" >
                             <a class="inline-flex items-center w-full text-sm font-semibold text-white transition-colors duration-150 cursor-pointer hover:text-green-500"
-                               href="index.php?page=products">
+                               href="index.php?page=categories">
                                     <span
                                             class="inline-flex items-center  text-sm font-semibold text-white hover:text-green-400">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
