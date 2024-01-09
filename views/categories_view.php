9b6a9733b4958fd41eef5fd38d637189cf2a6030
kholoud001 <kholoud.sanak@gmail.com>
2024-01-09 22:28:12 +0100

    category crud

--- views/categories_view.php
+++ views/categories_view.php
@@ -56,7 +56,7 @@
                 <div class="bg-white p-4 shadow-lg rounded-lg">
                     <div class="flex justify-between">
                         <h1 class="font-bold text-base">Table</h1>
-                        <button  id="addProductForm" class=" mb-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        <button  id="addcategory-btn" class=" mb-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                             Add Catgeory
                             </button>
                     </div>
@@ -95,7 +95,7 @@
                                                 </th>
                                             </tr>
                                             </thead>
-                                            <tbody id ="product-table" class="bg-white divide-y divide-gray-200">
+                                            <tbody id ="category-table" class="bg-white divide-y divide-gray-200">
                                             </tbody>
                                         </table>
                                     </div>
@@ -106,9 +106,84 @@
                 </div>
             </div>
         </div>
+        <?php include "views/categoryform_view.php";?>
+
     </div>
 </div>
 </div>
 </main>
 </div>
 </div>
+<script src="<?= PATH ?>assets/js/category.js"></script>
+
+
+
+<script>
+
+    function getCategories() {
+        $.ajax({
+            type: "POST",
+            url: "index.php?page=categories",
+            data: {   action: 'getCategories'},
+            success: function (data) {
+                let categories = JSON.parse(data);
+                //console.log(categories);
+
+                $('#category-table').empty();
+
+                categories.forEach(category => {
+                    let updated_at_display = category.updated_at !== null ? category.updated_at : 'Not updated yet';
+
+                    let row = `
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+                                <p>${category.category}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+                                <p>${category.created_at}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+                                    <p>${updated_at_display}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+                                <div class="flex space-x-4">
+                                    <button  id="edit-category" class="text-blue-500 hover:text-blue-600 "
+                                            data-category-id="${category.category_id}"
+                                            data-category="${category.category}"
+                                            >
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                        </svg>
+                                        <p>Edit</p>
+                                    </button>
+                                    <button  class=" delete_category text-red-500 hover:text-red-600"
+                                                data-category-id="${category.category_id}">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-1 ml-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                        </svg>
+                                        <p>Delete</p>
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    `;
+                    $('#category-table').append(row);
+                });
+            }
+        });
+    }
+    getCategories();
+
+
+
+
+
+
+
+
+
+
+
+
+
+</script>
