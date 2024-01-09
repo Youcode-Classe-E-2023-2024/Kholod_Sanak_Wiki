a85ee8609b381ac21e06c3b8250cd34e1f440058
kholoud001 <kholoud.sanak@gmail.com>
2024-01-09 22:26:51 +0100

    category crud

--- controllers/categories_controller.php
+++ controllers/categories_controller.php
@@ -1,1 +1,28 @@
 <?php
+//$category = new Category();
+//$categories = $category->getCategories();
+//if (isset($_POST["categories"])) {
+//echo json_encode($categories);
+//exit;
+//}
+//
+//
+// Handle the AJAX requests
+if (isset($_POST["action"])) {
+    $category = new Category();
+
+    if ($_POST["action"] === "getCategories") {
+        $categories = $category->getCategories();
+        echo json_encode($categories);
+        exit;
+    } elseif ($_POST["action"] === "delete") {
+        $categoryId = $_POST["categoryId"];
+        $result = $category->deleteCategory($categoryId);
+        if ($result) {
+            echo "success";
+            exit;
+        } else
+            exit;
+    }
+}
+
