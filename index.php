f84c923cd2fafa308b25592c6fec04d0c620361e
kholoud001 <kholoud.sanak@gmail.com>
2024-01-09 08:40:52 +0100

    update

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ spl_autoload_register(function ($class) {
 
 if (isset($_GET['page']) && !empty($_GET['page'])) {
     $page = trim(strtolower($_GET['page']));
+//    dd($page);
 } else {
     $page = 'home';
 }
@@ -18,7 +19,6 @@ if (isset($_GET['page']) && !empty($_GET['page'])) {
 $all_pages = scandir('controllers');
 
 if (in_array($page . '_controller.php', $all_pages)) {
-//    include_once 'models/' . $page . '_model.php';
     include_once 'controllers/' . $page . '_controller.php';
     include_once 'views/_layout.php';
 } else {
