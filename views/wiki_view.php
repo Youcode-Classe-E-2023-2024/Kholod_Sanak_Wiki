0ec66a2fc70d92da3b9b4bd0777eef457c402e00
kholoud001 <kholoud.sanak@gmail.com>
2024-01-09 08:40:07 +0100

    update

--- views/wiki_view.php
+++ views/wiki_view.php
@@ -1,7 +1,7 @@
 <?php
 ?>
 <header>
-    <?php include_once 'views/header_view.php'; ?>
+    <?php include_once 'views/components/_header.php'; ?>
 </header>
 <section class="text-gray-600 body-font">
     <div class="container px-5 py-24 mx-auto">
@@ -17,5 +17,5 @@
     </div>
 </section>
 <footer>
-    <?php include_once 'views/footer_view.php'; ?>
+    <?php include_once 'views/components/_footer.php'; ?>
 </footer>
