f1e305ebb39c521cc25638fd047755e7f63aa60b
kholoud001 <kholoud.sanak@gmail.com>
2024-01-09 08:40:44 +0100

    update

--- views/_layout.php
+++ views/_layout.php
@@ -7,6 +7,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= ucfirst($page) ?></title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+          rel="stylesheet" />
+
+
 
     <link rel="stylesheet" href="<?= PATH ?>assets/css/style.css">
 </head>
