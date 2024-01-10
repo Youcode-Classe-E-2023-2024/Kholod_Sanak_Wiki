713eaaf349647a6074d8f2d65def32d40fd3a479
kholoud001 <kholoud.sanak@gmail.com>
2024-01-10 17:28:01 +0100

    dashboard update

--- controllers/dashboard_controller.php
+++ controllers/dashboard_controller.php
@@ -1,1 +1,10 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["logout"])) {
+        $user = new User();
+        $user->logout();
+    }
+}
+if (empty($_SESSION['user_id'])) {
+    header('location: index.php?page=login');
+}
