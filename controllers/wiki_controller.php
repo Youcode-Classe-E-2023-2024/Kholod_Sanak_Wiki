d3a920966d7a70f19f56975fa3728f639b157443
kholoud001 <kholoud.sanak@gmail.com>
2024-01-11 20:40:35 +0100

    wiki content controller

--- controllers/wiki_controller.php
+++ controllers/wiki_controller.php
@@ -1,1 +1,15 @@
 <?php
+if (isset($_GET["wiki_id"]) && isset($_GET["action"])) {
+
+    $wikiModel = new Wiki();
+    $wikiId = $_GET['wiki_id'];
+
+    if ($_GET["action"] === "Content") {
+        $wiki = $wikiModel->displayWiki($wikiId);
+        echo json_encode($wiki);
+        exit;
+    }
+}
+
+
+
