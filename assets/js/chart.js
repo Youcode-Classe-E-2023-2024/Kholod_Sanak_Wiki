2ab8406e2d0b147f58818ae16450c514a91fc9ed
kholoud001 <kholoud.sanak@gmail.com>
2024-01-11 16:07:24 +0100

    chart js

---
+++ assets/js/chart.js
@@ -0,0 +1,23 @@
+
+
+function initializeChart(jsonData) {
+    // Chart options and data
+    var options = {
+        chart: {
+            type: 'bar'
+        },
+        series: [{
+            name: 'Number',
+            data: [jsonData.wikis, jsonData.categories, jsonData.tags]
+        }],
+        xaxis: {
+            categories: ['Wikis', 'Categories', 'Tags']
+        }
+    };
+
+    // Initialize the chart
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+    // Render the chart
+    chart.render();
+}
