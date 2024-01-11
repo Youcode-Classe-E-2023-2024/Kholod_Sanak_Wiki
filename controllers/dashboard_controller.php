35c72941a6bd168a8257f085023b429af4e8c38a
kholoud001 <kholoud.sanak@gmail.com>
2024-01-11 16:08:17 +0100

    dashboard controller update

--- controllers/dashboard_controller.php
+++ controllers/dashboard_controller.php
@@ -1,4 +1,22 @@
 <?php
+
+
+$wikiModel = new Wiki();
+$categoryModel = new Category();
+$tagModel = new Tag();
+
+
+$wikisNumber = $wikiModel->getWikisCount();
+$categoriesNumber = $categoryModel->getCategoriesCount();
+$tagsNumber = $tagModel->getTagsCount();
+
+$data = array(
+    'wikis' => $wikisNumber,
+    'categories' => $categoriesNumber,
+    'tags' => $tagsNumber
+);
+$jsonData = json_encode($data);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["logout"])) {
         $user = new User();
